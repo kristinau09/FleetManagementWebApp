10ac949826ce225d77f059d09f432b0e7d7e6b82
kristinau09 <kristinau09@gmail.com>
2021-02-24 11:37:14 -0600

    Fleet management of vehicles

---
+++ src/main/java/com/example/fleetManagementWebApp/FleetManagementWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fleetManagementWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fleetManagementWebApp/FleetManagementWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fleetManagementWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
