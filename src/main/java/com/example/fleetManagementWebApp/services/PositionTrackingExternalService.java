4f8dbffea891308b709fc9ed55f95453b4b67902
kristinau09 <kristinau09@gmail.com>
2021-02-24 20:32:40 -0600

    modification in external service method

--- src/main/java/com/example/fleetManagementWebApp/services/PositionTrackingExternalService.java
+++ src/main/java/com/example/fleetManagementWebApp/services/PositionTrackingExternalService.java
@@ -7,28 +7,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.fleetManagementWebApp.controllers.PositionOfVehicle;
+import com.example.fleetManagementWebApp.dao.VehicleRepository;
+import com.example.fleetManagementWebApp.domain.Vehicle;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 
 @Service
+@Transactional
 public class PositionTrackingExternalService {
 	
 	/*
 	 * this object will call eureka, and when it gets more than one instance back
-	 * from Eureka this object contains the intelligence to choose one of those
+	 * from Eureka this object will choose one of those
 	 * instances, not randomly, but using algorithm called round robin algorithm
 	 */
 	@Autowired
 	private LoadBalancerClient balancer;
 	
+	@Autowired
+	private VehicleRepository dao;
+	
 	/*
 	 * With the annotation, hystrix knows that this is a method that we want to provide a fallback for.
-	 * The fallback method handleExternalServiceDown() will run whenever this method thrown an exception inside it
+	 * The fallback method handleExternalServiceDown() will run whenever this method throws an exception inside it
 	 * The fallback method handleExternalServiceDown() needs to be in same class and the signature including
-	 *       return type needs to be same athis method
+	 *       return type needs to be same as this method
+	 *       
+	 * If the situation becomes dire, then this method will be automatically skipped and Hystrix will immediately go to the fallback   
 	 */
 
 	@HystrixCommand(fallbackMethod="handleExternalServiceDown")
@@ -52,8 +61,22 @@ public class PositionTrackingExternalService {
 		//call uri which returns the physical address where the service is located
 		String physicalLocation = service.getUri().toString();
 
-		//making a rest request of current position of that vehicle
+		/*
+		 * making a rest request of current position of that vehicle, network connection
+		 * is going to be made here and remote server is being called
+		 */
 		PositionOfVehicle response = restTemplate.getForObject(physicalLocation + "/vehicles/" + vehicleName, PositionOfVehicle.class);
+		
+		//save the position in our h2database
+		Vehicle vehicleFromDb = dao.findByVehicleName(vehicleName);
+		
+		vehicleFromDb.setLatitute(response.getlatitude());
+		vehicleFromDb.setLongitude(response.getLongitude());
+		vehicleFromDb.setLastRecordedPosition(response.getTimeStamp());
+		
+		//this object is dirty, when the transaction is committed, then JPA will dirty check the object
+		
+		
 		
 		System.out.println("SUCCESS!!!!!");
 		
@@ -65,11 +88,13 @@ public class PositionTrackingExternalService {
 		
 		System.out.println("Running fallback method:::::");
 		
-		//for now
+		//read the last known position for this vehicle
 		PositionOfVehicle position = new PositionOfVehicle();
-		position.setlatitude(new BigDecimal("423"));
-		position.setLongitude(new BigDecimal("0.0"));
-		position.setTimeStamp(new Date());
+		//find the vehicle
+		Vehicle vehicle = dao.findByVehicleName(vehicleName);
+		position.setlatitude(vehicle.getLatitute());
+		position.setLongitude(vehicle.getLongitude());
+		position.setTimeStamp(vehicle.getLastRecordedPosition());
 		
 		return position;
 	}
